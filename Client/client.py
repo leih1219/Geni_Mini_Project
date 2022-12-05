f19a11e34511407514771c5831c9d2810dc0ac0b
Leo <924984213@qq.com>
2022-12-04 22:16:18 -0500

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -25,8 +25,8 @@ def sendFiles():
         file = request.files['img']
         print('file_name:' + file.name)
         file.save(secure_filename(file.filename))
-        # remote_IP = '192.122.236.106'
-        remote_IP = 'http://127.0.0.1'
+        remote_IP = '192.122.236.106'
+#         remote_IP = 'http://127.0.0.1'
         remote_PORT = '5000'
         remote_Sub = 'predict'
         remote_add = remote_IP + ':' + remote_PORT + '/' + remote_Sub
