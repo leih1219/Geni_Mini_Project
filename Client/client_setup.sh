7c3bd8058e23d51f49dff0d9d5983b064e05ac57
Leo <924984213@qq.com>
2022-12-06 17:08:01 -0500

    Add files via upload

---
+++ Client/client_setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install -y python3-venv
+sudo apt install -y python3-pip
+
+python3 -m venv CS655
+. CS655/bin/activate
+
+pip install flask
+pip install requests
+
+git clone https://github.com/leih1219/Geni_Mini_Project.git
+
+cd Geni_Mini_Project/Client
